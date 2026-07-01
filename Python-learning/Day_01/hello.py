e747d6b1c0ecc164e3db92bb94817c6312f107c9
C.K.SARVESHWARAN <just4sarvesh@gmail.com>
2026-07-01 11:05:03 +0530

    Learn Python variables

--- Python-learning/Day_01/hello.py
+++ Python-learning/Day_01/hello.py
@@ -1,3 +1,24 @@
-print("Hello, GitHub!")
-print("My name is Sarvesh.")
-print("This is my first Git project.")
+name = "Sarvesh"
+
+age = 17
+
+college = "VIT Chennai"
+
+branch = "Electronics Engineering (VLSI Design & Technology)"
+
+print(name)
+print(age)
+print(college)
+print(branch)
+
+Fav_F1_Driver = "Max Verstappen"
+Fav_no = 6
+Fav_Company = "Red Bull Racing"
+
+print(Fav_F1_Driver)
+print(Fav_no)
+print(Fav_Company)
+
+print(name,college)
+
+print(name, age, college, branch)
